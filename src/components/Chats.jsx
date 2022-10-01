4c00da14a12b9f3be7cc26b0969a9e9018e05f8c
ngtinh <nguyenvanductinh77@gmail.com>
2022-10-01 16:44:43 +0700

    Design chats

--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -2,7 +2,37 @@ import React from 'react'
 
 const Chats = () => {
   return (
-    <div>Chats</div>
+    <div className='chats'>
+      <div className="userChat">
+        <img src="https://scontent.fsgn5-6.fna.fbcdn.net/v/t39.30808-6/274615944_1400460803744919_641614232942927374_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=DbaZXkwdsPEAX8QSJCe&_nc_ht=scontent.fsgn5-6.fna&oh=00_AT-6RSdooTNyXHuuMNHTfIgOlHEVJ_j6jcL2vRKjdQHX-A&oe=633CCE8D" alt="day la ngiu tinh" />
+        <div className="userChatInfo">
+          <span>Tinhs</span>
+          <p>Hello</p>
+        </div>
+      </div>
+      <div className="userChat">
+        <img src="https://scontent.fsgn5-6.fna.fbcdn.net/v/t39.30808-6/274615944_1400460803744919_641614232942927374_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=DbaZXkwdsPEAX8QSJCe&_nc_ht=scontent.fsgn5-6.fna&oh=00_AT-6RSdooTNyXHuuMNHTfIgOlHEVJ_j6jcL2vRKjdQHX-A&oe=633CCE8D" alt="day la ngiu tinh" />
+        <div className="userChatInfo">
+          <span>Tinhs</span>
+          <p>Hello</p>
+        </div>
+      </div>
+      <div className="userChat">
+        <img src="https://scontent.fsgn5-6.fna.fbcdn.net/v/t39.30808-6/274615944_1400460803744919_641614232942927374_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=DbaZXkwdsPEAX8QSJCe&_nc_ht=scontent.fsgn5-6.fna&oh=00_AT-6RSdooTNyXHuuMNHTfIgOlHEVJ_j6jcL2vRKjdQHX-A&oe=633CCE8D" alt="day la ngiu tinh" />
+        <div className="userChatInfo">
+          <span>Tinhs</span>
+          <p>Hello</p>
+        </div>
+      </div>
+      <div className="userChat">
+        <img src="https://scontent.fsgn5-6.fna.fbcdn.net/v/t39.30808-6/274615944_1400460803744919_641614232942927374_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=DbaZXkwdsPEAX8QSJCe&_nc_ht=scontent.fsgn5-6.fna&oh=00_AT-6RSdooTNyXHuuMNHTfIgOlHEVJ_j6jcL2vRKjdQHX-A&oe=633CCE8D" alt="day la ngiu tinh" />
+        <div className="userChatInfo">
+          <span>Tinhs</span>
+          <p>Hello</p>
+        </div>
+      </div>
+    </div>
+    
   )
 }
 
